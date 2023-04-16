24ffb6d9514da8c78e797667d901960fb330266b
IbrahimGanaMalgwi <ibrahimganamalgwi@outlook.com>
2023-04-16 20:08:19 +0100

    Add @crossOrigin annotation

--- src/main/java/com/ganzymalgwi/employeesystemapi/controller/EmployeeController.java
+++ src/main/java/com/ganzymalgwi/employeesystemapi/controller/EmployeeController.java
@@ -2,11 +2,9 @@ package com.ganzymalgwi.employeesystemapi.controller;
 
 import com.ganzymalgwi.employeesystemapi.model.Employee;
 import com.ganzymalgwi.employeesystemapi.services.EmployeeService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
