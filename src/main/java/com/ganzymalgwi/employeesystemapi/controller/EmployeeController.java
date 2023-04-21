47d17b3f449588554189985fe3817eb9316c5703
IbrahimGanaMalgwi <ibrahimganamalgwi@outlook.com>
2023-04-21 12:01:05 +0100

    update employee

--- src/main/java/com/ganzymalgwi/employeesystemapi/controller/EmployeeController.java
+++ src/main/java/com/ganzymalgwi/employeesystemapi/controller/EmployeeController.java
@@ -39,5 +39,20 @@ public class EmployeeController {
         response.put("deleted", deleted);
         return ResponseEntity.ok(response);
     }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        Employee employee = null;
+        employee = employeeService.getEmployeeById(id);
+        return  ResponseEntity.ok(employee);
+    }
+
+    @PutMapping("/employess/{id}")
+    public ResponseEntity<Employee> updateEmploye(@PathVariable  Long id,
+                                                  @RequestBody Employee employee){
+        employee = employeeService.updateEmployee(id, employee);
+        return ResponseEntity.ok(employee);
+
+    }
 }
 
--- src/main/java/com/ganzymalgwi/employeesystemapi/services/EmployeeServiceImpl.java
+++ src/main/java/com/ganzymalgwi/employeesystemapi/services/EmployeeServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
@@ -46,5 +45,24 @@ public class EmployeeServiceImpl implements EmployeeService{
         EmployeeEntity employee = employeeRepository.findById(id).get();
         employeeRepository.delete(employee);
         return true;
+    }
+
+    @Override
+    public Employee getEmployeeById(Long id) {
+        EmployeeEntity employeeEntity = employeeRepository.findById(id).get();
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeEntity, employee);
+        return  employee;
+    }
+
+    @Override
+    public Employee updateEmployee(Long id, Employee employee) {
+        EmployeeEntity employeeEntity = employeeRepository.findById(id).get();
+        employeeEntity.setFirstName(employee.getFirstName());
+        employeeEntity.setLastName(employee.getLastName());
+        employeeEntity.setEmail(employee.getEmail());
+
+        employeeRepository.save(employeeEntity);
+        return employee;
     }
 }
